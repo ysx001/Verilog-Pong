36489834bdebd5d37ab6237447c608cb0e03fa66
ysx001 <ysx001@bucknell.edu>
2016-04-10 03:30:00 -0400

    Update game.v

--- game.v
+++ game.v
@@ -1,17 +1,49 @@
 module game(
-		input [9:0] X // the length of this reg depends on the length of the output of the VGA
-		input [9:0] Y // the length of this reg depends on the length of the output of the VGA
-		output //?? ARHTUNG!!!!!!!  reserved
+		input clk25M, reset, vga_on;
+		input [9:0] x, y; // the length of this reg depends on the length of the output of the VGA
+		output [2:0] rgb; // outputs the rgb color value for the ball
 		)	
 
-//part: the movent of the ball
-	reg [9:0] ballX; //the x-pos of the ball
-	reg [9:0] ballY; //the y-pos of the ball
+	
+	
+	// Define a size for the ball
+	localparam ball_size = 10;
+	wire [9:0] ball_left, ball_right, ball_top, ball_bottom;
+	
+	reg [9:0] ball_x, ball_y;
+	wire [9:0] ball_x_next, ball_y_next;
+	
+	reg [9:0] diff_x, diff_y; // difference in x and y -> for tracking x_vel and y_vel
+	reg [9:0] diff_x_next, diff_y_next; // 
+	
+	
 	reg ball_dirX, ball_dirY; //1=right&up, 0=left&down
 	reg bounceX, bounceY; //1=right&up, 0=left&down
 
-	wire endofframe = (??) //depends on how the video is being scanned
-
+	wire endofframe = (x == 0 && y == 481); //depends on how the video is being scanned
+	// end of frame when raster reach the beginning of last line
+	
+	
+	always @ (posedge clk25M, posedge reset)
+		if (reset) begin
+			ball_x <= 0;
+			ball_y <= 0;
+			diff_x <= 0;
+			diff_y <= 0;
+		  end
+		else
+		  begin
+			ball_x <= ball_x_next;
+			ball_y <= ball_y_next;
+			diff_x <= diff_x_next;
+			diff_y <= diff_y_next;
+		  end
+			
+	
+	
+	// ball movement
+	
+	assign ball_left = ballX;
 	always @(posedge clk) begin
 
 		if(endofframe) //when a frame has ended
