a2070a824a10999e0367b6502328002580458fed
loll70 <hc017@bucknell.edu>
2016-04-14 19:11:52 -0400

    added debouncer still can't test successfully

--- Score.v
+++ Score.v
@@ -50,11 +50,16 @@ module seven_seg(input clk25,
 				digitselect = ~4'b0010;
 				A = second;
 			end
-			default :
+			2'b10 :
+			begin
+				digitselect = ~4'b0100;
+				A = 2'b00;
+			end
+			2'b11 :
 			begin
 				digitselect = ~4'b1000;
 				A = 2'b00;
-			end			
+			end
 		endcase
 		
 HexTo7Seg myencoder(A, segments);
@@ -68,16 +73,16 @@ module HexTo7Seg(
 	
 	always @(*)
 		case(A)			
-			4'h0 : SevenSegValue = ~8'b11111100;  // Note inversion
-			4'h1 : SevenSegValue = ~8'b01100000;
-			4'h2 : SevenSegValue = ~8'b11011010;
-			4'h3 : SevenSegValue = ~8'b11110010;
-			4'h4 : SevenSegValue = ~8'b01100110;
-			4'h5 : SevenSegValue = ~8'b10110110;
-			4'h6 : SevenSegValue = ~8'b10111110;
-			4'h7 : SevenSegValue = ~8'b11100000;
-			4'h8 : SevenSegValue = ~8'b11111110;
-			4'h9 : SevenSegValue = ~8'b11110110;
+			4'd0 : SevenSegValue = ~8'b11111100;  // Note inversion
+			4'd1 : SevenSegValue = ~8'b01100000;
+			4'd2 : SevenSegValue = ~8'b11011010;
+			4'd3 : SevenSegValue = ~8'b11110010;
+			4'd4 : SevenSegValue = ~8'b01100110;
+			4'd5 : SevenSegValue = ~8'b10110110;
+			4'd6 : SevenSegValue = ~8'b10111110;
+			4'd7 : SevenSegValue = ~8'b11100000;
+			4'd8 : SevenSegValue = ~8'b11111110;
+			4'd9 : SevenSegValue = ~8'b11110110;
 			default : SevenSegValue = ~ 8'b10001110;
 		endcase
 endmodule
